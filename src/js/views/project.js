4df04668f3030722869578d2c614d4886a56b52d
Shane Bryan <shane.bryan@intel.com>
2012-04-20 09:27:14 -0700

    [General] Derive all View widgets from baseView plugin
    The signficant portion of this change involved the following changes
    (with some slight variations) to most of the view classes:
    - Subclass from $.rib.baseView
    - All model binding and event callback hooking is done in baseView now
    - Primary and secondary tool creation is tiggered in baseView
    - Added _init() to perform the refresh() call rather than in _create()
    - Use chaining in _create() and _setOption() to get default capabilities
      while allowing for local overrides/changes/actions
    - Removed (or chained) duplicate functions now provided by baseView
      prototype object
      * _create()
      * destroy()
      * _bindADMEvents()
      * _unbindADMEvents()
      * _designResetHandler()
    
    outline.js also had similar changes, but since it is now a "grandchild"
    of baseView, with prototyping through treeView as well, there are some
    slight variations to the above, though substantially the same.
    
    The file "view-core.js" has been renamed to "base.js"
    
    Also made a change in composer.js to gracefully handle unfound node in ADM
    
    Signed-off-by: Shane Bryan <shane.bryan@intel.com>

--- src/js/views/project.js
+++ src/js/views/project.js
@@ -13,32 +13,17 @@
 
 (function($, undefined) {
 
-    $.widget('rib.projectView', {
-
-        options: {
-            model: null,
-        },
+    $.widget('rib.projectView', $.rib.baseView, {
 
         _create: function() {
             var o = this.options,
                 e = this.element,
                 widget = this;
 
-            o.designReset = this._designResetHandler;
-            o.selectionChanged = null;
-            o.activePageChanged = null;
-            o.modelUpdated = this._modelUpdatedHandler;
-
-            // FIXME: This should work, but $.extend of options seems to be
-            //        creating a copy of the ADM, which will not containt the
-            //        same nodes and events as the master
-            //o.model = o.model || ADM || undefined;
-            if (o.model) {
-                this._bindADMEvents(o.model);
-            }
+            // Chain up to base class _create()
+            $.rib.baseView.prototype._create.call(this);
 
             $('<div/>').addClass(this.widgetName)
-                .text(this.widgetName)
                 .appendTo(this.element);
 
             // Add a hidden file input element, used to trigger the browsers
@@ -50,14 +35,9 @@
                 .change(widget._importChangedCallback)
                 .appendTo(this.element[0].ownerDocument.body);
 
-            // Add a project setting dialog element, used to trigger to configure
-            // project setting when user want to create or modify project setting
+            // Add a project setting dialog element, used to configure
+            // project setting at creation, or to modify them later.
             this.options.projectDialog =  this._createSettingDialog();
-
-            this.options.primaryTools = this._createPrimaryTools();
-            this.options.secondaryTools = this._createSecondaryTools();
-
-            this.refresh(null, this);
 
             return this;
         },
@@ -80,13 +60,15 @@
             // Clear the value
             $(e.currentTarget).val('');
         },
+
         _setOption: function(key, value) {
+            // Chain up to base class _setOptions()
+            // FIXME: In jquery UI 1.9 and above, instead use
+            //    this._super('_setOption', key, value)
+            $.rib.baseView.prototype._setOption.apply(this, arguments);
+
             switch (key) {
-                // Should this REALLY be done here, or plugin registration in
-                // the "host"... using the functions mapped in widget options?
                 case 'model':
-                    this._unbindADMEvents();
-                    this._bindADMEvents(value);
                     this.refresh(null, this);
                     break;
                 default:
@@ -94,17 +76,10 @@
             }
         },
 
-        destroy: function() {
-            // TODO: unbind any ADM event handlers
-            $(this.element).find('.'+this.widgetName).remove();
-            this.options.primaryTools.remove();
-            this.options.secondaryTools.remove();
-        },
-
         refresh: function(event, widget) {
             var pidArr, stage, container;
             widget = widget || this;
-            container = $('.projectView');
+            container = $('.projectView').last();
             container.empty();
             //get project counts from PM-API
             pidArr = $.rib.pmUtils.listAllProject();
@@ -201,92 +176,6 @@
             return [];
         },
 
-        _bindADMEvents: function(a) {
-            var o = this.options,
-                d = this.designRoot;
-
-            if (a) {
-                o.model = a;
-
-                if (o.designReset) {
-                    a.bind("designReset", o.designReset, this);
-                }
-                if (o.selectionChanged) {
-                    a.bind("selectionChanged", o.selectionChanged, this);
-                }
-                if (o.activePageChanged) {
-                    a.bind("activePageChanged", o.activePageChanged, this);
-                }
-
-                // Since model changed, need to call our designReset hander
-                // to sync up the ADMDesign modelUpdated event handler
-                if (o.designReset) {
-                    o.designReset({design: a.getDesignRoot()}, this);
-                }
-            }
-        },
-
-        _unbindADMEvents: function() {
-            var o = this.options,
-                a = this.options.model,
-                d = this.designRoot;
-
-            // First unbind our ADMDesign modelUpdated handler, if any...
-            if (d && o.modelUpdated) {
-                d.designRoot.unbind("modelUpdated", o.modelUpdated, this);
-            }
-
-            // Now unbind all ADM model event handlers, if any...
-            if (a) {
-                if (o.designReset) {
-                    a.unbind("designReset", o.designReset, this);
-                }
-                if (o.selectionChanged) {
-                    a.unbind("selectionChanged", o.selectionChanged, this);
-                }
-                if (o.activePageChanged) {
-                    a.unbind("activePageChanged", o.activePageChanged, this);
-                }
-            }
-        },
-
-        _designResetHandler: function(event, widget) {
-            var d = event && event.design, o;
-
-            widget = widget || this;
-            o = widget.options;
-            d = d || o.model.getDesignRoot();
-
-            // Do nothing if the new ADMDesign equals our currently cached one
-            if (d === widget.designRoot) {
-                return;
-            }
-
-            // First, unbind existing modelUpdated hander, if any...
-            if (widget.designRoot && o.modelUpdated) {
-                widget.designRoot.unbind("modelUpdated", o.modelUpdated,widget);
-            }
-
-            // Next, bind to modelUpdated events from new ADMDesign, if any...
-            if (d && o.modelUpdated) {
-                d.bind("modelUpdated", o.modelUpdated, widget);
-            }
-
-            // Then, cache the new ADMDesign reference with this instance
-            widget.designRoot = d;
-
-            // Finally, redraw our view since the ADMDesign root has changed
-            widget.refresh(event, widget);
-        },
-
-        _selectionChangedHandler: function(event, widget) {
-            widget = widget || this;
-        },
-
-        _activePageChangedHandler: function(event, widget) {
-            widget = widget || this;
-        },
-
         _modelUpdatedHandler: function(event, widget) {
             widget = widget || this;
             // if the designDirty is false, then set it
@@ -370,9 +259,10 @@
         },
 
         createProjectBox: function(pid, container, widget) {
-            var box, title, content, thumbnail, imgPath, rightSide, openHandler, cloneHandler, deleteHandler;
+            var box, title, content, thumbnail, imgPath, rightSide,
+                openHandler, cloneHandler, deleteHandler;
             widget = widget || this;
-            container = container || $('.projectView');
+            container = container || $('.projectView').last();
             openHandler = function () {
                 var success = function () {
                     // show the layout tab
