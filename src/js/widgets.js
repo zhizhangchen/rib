7c0d5b32098450829292d6be7646cac63195573b
yongfeng wu <yongfengx.wu@intel.com>
2012-05-30 14:46:03 +0800

    [Widgets] Support image elements using <img> tag

--- src/js/widgets.js
+++ src/js/widgets.js
@@ -496,6 +496,54 @@ var BWidgetRegistry = {
     },
 
     /**
+     * Represents a image
+     */
+    Image: {
+        parent: "Base",
+        paletteImageName: "jqm_image.svg",
+        template: function(node) {
+            var prop, code = $('<img/>');
+            code = BWidgetRegistry.Base.applyProperties(node, code);
+            if (node.getProperty("align") === "left") {
+                code.attr("style", "display:block;margin:auto auto auto 0px ");
+            } else if (node.getProperty("align") === "center") {
+                code.attr("style", "display:block;margin: 0 auto");
+            } else if (node.getProperty("align") === "right") {
+                code.attr("style", "display:block;margin: auto 0px auto auto ");
+            }
+            return code;
+        },
+        properties: {
+            src: {
+                type: "string",
+                defaultValue: "src/css/images/widgets/jqm_image.svg",
+                htmlAttribute: "src",
+                forceAttribute: true
+            },
+            alt: {
+                type: "string",
+                defaultValue: "",
+                htmlAttribute: "alt"
+            },
+            width: {
+                type: "string",
+                defaultValue: "",
+                htmlAttribute: "width"
+            },
+            height: {
+                type: "string",
+                defaultValue: "",
+                htmlAttribute: "height"
+            },
+            align: {
+                type: "string",
+                options:[ "left", "center", "right" ],
+                defaultValue: "left",
+            },
+        },
+    },
+
+    /**
      * Represents an HTML form object. Includes an "action" property with the
      * submission URL and a "method" string property that should be "get" or
      * "post".
