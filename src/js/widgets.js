fbdccc38efa29fa19b734a18d9baa7df2d10961c
Chao Liu <chao.c.liu@intel.com>
2012-08-29 04:33:45 +0800

    [Property View] Fix bug: Grid uses two types of form widget for interger

--- src/js/widgets.js
+++ src/js/widgets.js
@@ -1746,18 +1746,12 @@ var BWidgetRegistry = {
             rows: {
                 type: "integer",
                 defaultValue: 1,
+                range: "1-100",
                 setPropertyHook: function (node, value, transactionData) {
                     var rows, columns, i, block, map, children, blocks, count,
                         blockIndex, root;
                     rows = node.getProperty("rows");
                     columns = node.getProperty("columns");
-
-                    // FIXME: really this should be enforced in the property
-                    //        pane, or elsewhere; this won't really work
-                    if (value < 1) {
-                        value = 1;
-                    }
-
                     root = node.getDesign();
                     root.suppressEvents(true);
 
@@ -1811,8 +1805,8 @@ var BWidgetRegistry = {
             },
             columns: {
                 type: "integer",
-                options: [ 2, 3, 4, 5 ],
                 defaultValue: 2,
+                range: "2-5",
                 setPropertyHook: function (node, value, transactionData) {
                     var rows, columns, i, block, map, children, blocks, count,
                         index, blockIndex, root;
@@ -2526,6 +2520,21 @@ var BWidget = {
         var schema = BWidget.getPropertySchema(widgetType, property);
         if (schema) {
             return schema.htmlValueMap;
+        }
+        return schema;
+    },
+
+    /**
+     * Gets the range for a given property.
+     *
+     * @param {String} widgetType The type of the widget.
+     * @param {String} property The name of the requested property.
+     * @return {String} The range for the given property
+     */
+    getPropertyRange: function (widgetType, property) {
+        var schema = BWidget.getPropertySchema(widgetType, property);
+        if (schema) {
+            return schema.range;
         }
         return schema;
     },
